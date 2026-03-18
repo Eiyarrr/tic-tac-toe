6f39e77a522f5ca2b76064972b85fba1290101e2
Eiyarrr <eiyar@eiyar.com>
2026-03-18 08:33:50 +0000

    Added basic board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// 0, 1, 2 -> _, X, O
 fn main() {
-    println!("Hello, world!");
+    let mut board: [i8; 9] = [
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+    ];
+}
+
+fn check_victory(board: &[i8; 9]) -> i8 {
+    0
 }
