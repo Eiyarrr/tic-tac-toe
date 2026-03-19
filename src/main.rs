98b6f50d006406d73f52028f27a83c25decb1cf8
Eiyarrr <eiyar@eiyar.com>
2026-03-19 03:00:18 +0000

    Added basic game loop & loop functions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 // 0, 1, 2 -> _, X, O
 fn main() {
-    let mut board: [i8; 9] = [
-        0, 0, 0,
-        0, 0, 0,
-        0, 0, 0
+    let mut board: [char; 9] = [
+        '_', '_', '_',
+        '_', '_', '_',
+        '_', '_', '_',
     ];
+
+    let mut turn: i8 = 0;   // Evens -> X, Odds -> O
+    loop {
+        board = make_move(&turn, board);
+        check_victory(&board);
+        turn += 1;
+    }
 }
 
-fn check_victory(board: &[i8; 9]) -> i8 {
+// 0 -> No victory
+// 1 -> X wins
+// 2 -> O wins
+// 5 -> Draw
+fn check_victory(board: &[char; 9]) -> i8 {
     0
+}
+
+fn make_move(turn: &i8, board: [char; 9]) -> [char; 9] {
+    board
 }
